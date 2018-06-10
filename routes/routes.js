a046335e5556b5d701a096339be7a094a53dfc68
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-06-09 19:29:32 -0700

    added authentication route, dashboard/user, and logging out

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,43 @@
-var routes = function(app) {
-  
-  app.get('/', (req, res) => {
-    res.render('index.html');
+var routes = function(app, passport) {
+
+  app.get('/auth/github', passport.authenticate('github'));
+
+  app.get('/auth/github/callback',
+    passport.authenticate('github', {
+      failureRedirect: '/',
+      session: true
+     }),
+     function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard/' + req.user._json.login);
   });
 
+  app.get('/', (req, res) => {
+    res.render('index.html', { user: req.user });
+  });
+
+  app.get('/dashboard/:user', isAuthenticated, function(req, res) {
+    res.render('dashboard.html', {
+      user: req.user
+    });
+  });
+
+  app.get('/logout', function(req, res){
+    console.log('logging out');
+    req.logout(); // passport terminates session
+    res.redirect('/');
+  });
+
+};
+
+// middleware to check if user is authenticatd
+var isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    console.log('User is not authenticated');
+    res.redirect('/');
+  }
 };
 
 module.exports = routes;
