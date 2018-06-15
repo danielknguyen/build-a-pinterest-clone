636d19268442d95d4515b8e85cec399aee7e51e6
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-06-14 22:12:51 -0700

    update

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "https://fcc-photo-share.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
